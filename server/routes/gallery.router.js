3ec78367db9cda81ed4557e7ae2a73f2d8f0129b
Ross Hutchens <rlhcxm@gmail.com>
2020-07-12 15:39:35 -0500

    Updated PUT route in gallery.router

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -5,18 +5,6 @@ const pool = require('../modules/pool.js');
 
 // DO NOT MODIFY THIS FILE FOR BASE MODE
 
-// // old PUT Route
-// router.put('/like/:id', (req, res) => {
-//     console.log(req.params);
-//     const galleryId = req.params.id;
-//     for(const galleryItem of galleryItems) {
-//         if(galleryItem.id == galleryId) {
-//             galleryItem.likes += 1;
-//         }
-//     }
-//     res.sendStatus(200);
-// }); // END PUT Route
-
 // PUT Route
 router.put('/like/:id', (req, res) => {
     console.log(req.params);
